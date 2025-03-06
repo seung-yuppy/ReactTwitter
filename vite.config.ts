740ef7c0b14ce30aae9f50c98baef02227e75d08
seung-yuppy <songsy0107@gmail.com>
2025-03-06 13:16:25 +0900

    25/03/06 세팅 최적화

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: '/src' },
-      { find: 'node_modules', replacement: '/node_modules' }
-    ]
-  }
+  plugins: [react()]
 });
